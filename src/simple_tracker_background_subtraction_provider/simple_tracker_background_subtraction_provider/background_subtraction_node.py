1d05c84d9104afcd10af52730b739418796887c1
Michael Groenewald <michael.groenewald@gmail.com>
2022-10-02 17:04:56 +0000

    Background subtraction and Cameta nodes

--- src/simple_tracker_background_subtraction_provider/simple_tracker_background_subtraction_provider/background_subtraction_node.py
+++ src/simple_tracker_background_subtraction_provider/simple_tracker_background_subtraction_provider/background_subtraction_node.py
@@ -1,43 +1,42 @@
 import datetime
 import rclpy
 import cv2
-from rclpy.node import Node
-from sensor_msgs.msg import Image
+from typing import List
 from cv_bridge import CvBridge
 from simple_tracker_interfaces.msg import Frame
-from simple_tracker_interfaces.msg import ConfigEntryUpdatedArray
-from simple_tracker_shared.config_entry_convertor import ConfigEntryConvertor
-from simple_tracker_shared.configurations_client_async import ConfigurationsClientAsync
+from simple_tracker_shared.configured_node import ConfiguredNode
 from .background_subtractor import BackgroundSubtractor
 
-class BackgroundSubtractionProviderNode(Node):
+class BackgroundSubtractionProviderNode(ConfiguredNode):
 
   def __init__(self):
 
     super().__init__('sky360_foreground_mask_provider')  
 
-    self.configuration_list = ['tracker_detection_sensitivity', 'background_subtractor_cuda_enable']
-    self.app_configuration = {}
-    self.configuration_loaded = False
-
     # setup services, publishers and subscribers
-    self.configuration_svc = ConfigurationsClientAsync()
     self.sub_grey_frame = self.create_subscription(Frame, 'sky360/frames/grey/v1', self.grey_frame_callback, 10)
     self.pub_foreground_mask_frame = self.create_publisher(Frame, 'sky360/frames/foreground_mask/v1', 10)
     self.pub_masked_background_frame = self.create_publisher(Frame, 'sky360/frames/masked_background/v1', 10)
-    self.sub_config_updated = self.create_subscription(ConfigEntryUpdatedArray, 'sky360/config/updated/v1', self.config_updated_callback, 10)
 
     # setup timer and other helpers
     self.br = CvBridge()
+    self.background_subtractor = BackgroundSubtractor.Select(self.app_configuration)
 
     self.get_logger().info(f'{self.get_name()} node is up and running.')
-   
-  def grey_frame_callback(self, data:Frame):
 
-    # TODO: This configuration update thing needs to happen in the background
-    if not self.configuration_loaded:
-      self._load_and_validate_config()
-      self.configuration_loaded = True
+  def config_list(self) -> List[str]:
+    return ['tracker_detection_sensitivity', 'background_subtractor_cuda_enable']
+
+  def validate_config(self) -> bool:
+    valid = True
+
+    if self.app_configuration['tracker_detection_sensitivity'] == None:
+      self.get_logger().error('The tracker_detection_sensitivity config entry is null')
+      valid = False
+      
+    return valid
+
+  def grey_frame_callback(self, data:Frame):
 
     frame_grey = self.br.imgmsg_to_cv2(data.frame)
 
@@ -57,46 +56,6 @@ class BackgroundSubtractionProviderNode(Node):
     frame_masked_background_msg.frame_count = data.frame_count
     frame_masked_background_msg.frame = self.br.cv2_to_imgmsg(frame_masked_background)
     self.pub_masked_background_frame.publish(frame_masked_background_msg)
-
-  def config_updated_callback(self, msg:ConfigEntryUpdatedArray):
-
-    for key in msg.keys:
-      if key in self.app_configuration.keys():
-        self.configuration_loaded = False
-        self.get_logger().info('Receiving updated configuration notification, reload')
-        break
-
-  def _load_and_validate_config(self):
-
-      if not self.configuration_loaded:
-        self._load_config()
-        
-        # TODO: What is the best way of exiting out of a launch script when the configuration validation fails
-        valid = self._validate_config()
-        if valid == False:
-          self.get_logger().error('Frame Provider configuration is invalid')
-
-        self.background_subtractor = BackgroundSubtractor.Select(self.app_configuration)
-
-        self.configuration_loaded = True
-
-  def _load_config(self):
-    #self.get_logger().info(f'Loading configuration list.')
-
-    response = self.configuration_svc.send_request(self.configuration_list)
-    for config_item in response.entries:
-      self.app_configuration[config_item.key] = ConfigEntryConvertor.Convert(config_item.type, config_item.value)
-
-  def _validate_config(self):
-    #self.get_logger().info(f'Validating configuration.')
-
-    valid = True
-
-    if self.app_configuration['tracker_detection_sensitivity'] == None:
-      self.get_logger().error('The tracker_detection_sensitivity config entry is null')
-      valid = False
-      
-    return valid
 
 def main(args=None):
 
--- src/simple_tracker_camera/simple_tracker_camera/camera_node.py
+++ src/simple_tracker_camera/simple_tracker_camera/camera_node.py
@@ -2,44 +2,50 @@ import datetime
 import rclpy
 import cv2
 import time
+from typing import List
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from simple_tracker_interfaces.msg import CameraFrame
-from simple_tracker_interfaces.msg import ConfigEntryUpdatedArray
-from simple_tracker_shared.config_entry_convertor import ConfigEntryConvertor
-from simple_tracker_shared.configurations_client_async import ConfigurationsClientAsync
+from simple_tracker_shared.configured_node import ConfiguredNode
 from simple_tracker_shared.utils import frame_resize
 
-class CameraNode(Node):
+class CameraNode(ConfiguredNode):
 
   def __init__(self):
 
     super().__init__('sky360_camera')  
 
-    self.configuration_list = ['camera_mode', 'camera_uri', 'camera_resize_dimension_h', 'camera_resize_dimension_w', 'camera_resize_frame', 'camera_cuda_enable']
-    self.app_configuration = {}
-    self.configuration_loaded = False
-
     # setup services, publishers and subscribers
-    self.configuration_svc = ConfigurationsClientAsync()
     self.pub_frame = self.create_publisher(CameraFrame, 'sky360/camera/original/v1', 10)    
-    self.sub_config_updated = self.create_subscription(ConfigEntryUpdatedArray, 'sky360/config/updated/v1', self.config_updated_callback, 10)
 
     # setup timer and other helpers
     timer_period = 0.1  # seconds
     self.timer = self.create_timer(timer_period, self.capture_timer_callback)
     self.br = CvBridge()
 
-    self.get_logger().info(f'{self.get_name()} node is up and running.')
-   
-  def capture_timer_callback(self):
+    self.capture = cv2.VideoCapture(self.app_configuration['camera_uri'])
 
-    # TODO: This configuration update thing needs to happen in the background
-    if not self.configuration_loaded:
-      self._load_and_validate_config()
-      self.capture = cv2.VideoCapture(self.app_configuration['camera_uri'])
-      self.configuration_loaded = True
+    self.get_logger().info(f'{self.get_name()} node is up and running.')
+
+  def config_list(self) -> List[str]:
+    return ['camera_mode', 'camera_uri', 'camera_resize_dimension_h', 'camera_resize_dimension_w', 'camera_resize_frame', 'camera_cuda_enable']
+
+  def validate_config(self) -> bool:
+    valid = True
+
+    if self.app_configuration['camera_uri'] == None:
+      self.get_logger().error('The camera_uri config entry is null')
+      valid = False
+
+    if self.app_configuration['camera_resize_frame']:
+      if self.app_configuration['camera_resize_dimension_h'] is None and self.app_configuration['camera_resize_dimension_w'] is None:
+        self.get_logger().error('Both camera_resize_dimension_h and camera_resize_dimension_w config entries are null')
+        valid = False
+
+    return valid
+
+  def capture_timer_callback(self):
 
     timer = cv2.getTickCount()
     success, frame = self.capture.read()
@@ -54,47 +60,6 @@ class CameraNode(Node):
       camera_frame_msg.frame = self.br.cv2_to_imgmsg(frame)
 
       self.pub_frame.publish(camera_frame_msg)
-
-  def config_updated_callback(self, msg):
-
-    for key in msg.keys:
-      if key in self.app_configuration.keys():
-        self.configuration_loaded = False
-        self.get_logger().info('Receiving updated configuration notification, reload')
-        break
-  
-  def _load_and_validate_config(self):
-
-      if not self.configuration_loaded:
-        self._load_config()
-        
-        # TODO: What is the best way of exiting out of a launch script when the configuration validation fails
-        valid = self._validate_config()
-        if valid == False:
-          self.get_logger().error('Camera configuration is invalid')
-
-  def _load_config(self):
-    # self.get_logger().info(f'Loading configuration list.')
-
-    response = self.configuration_svc.send_request(self.configuration_list)
-    for config_item in response.entries:
-      self.app_configuration[config_item.key] = ConfigEntryConvertor.Convert(config_item.type, config_item.value)
-
-  def _validate_config(self):
-    #self.get_logger().info(f'Validating configuration.')
-
-    valid = True
-
-    if self.app_configuration['camera_uri'] == None:
-      self.get_logger().error('The camera_uri config entry is null')
-      valid = False
-
-    if self.app_configuration['camera_resize_frame']:
-      if self.app_configuration['camera_resize_dimension_h'] is None and self.app_configuration['camera_resize_dimension_w'] is None:
-        self.get_logger().error('Both camera_resize_dimension_h and camera_resize_dimension_w config entries are null')
-        valid = False
-
-    return valid
 
 def main(args=None):
 
